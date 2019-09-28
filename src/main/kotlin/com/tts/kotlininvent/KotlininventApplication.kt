9c5657b4d712bcce8cea75f4f5259e7747cb952b
LionelBeato <lionel.beato@gmail.com>
2019-09-27 23:44:35 -0400

    adds proper put method!

--- src/main/kotlin/com/tts/kotlininvent/KotlininventApplication.kt
+++ src/main/kotlin/com/tts/kotlininvent/KotlininventApplication.kt
@@ -5,20 +5,36 @@ import com.tts.kotlininvent.repo.FruitRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @SpringBootApplication
-class KotlininventApplication{
+class KotlininventApplication {
 
 
-    //code that resolves at runtime that adds items to the repository
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("http://localhost:4200")
+        config.allowedMethods = listOf("*")
+        config.allowedHeaders = listOf("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
+
     @Bean
     fun init(repository: FruitRepository) = CommandLineRunner {
         repository.save(Fruit("apples", "5"))
         repository.save(Fruit("mangoes", "2"))
         repository.save(Fruit("pears", "3"))
-
-
     }
 }
 
--- src/main/kotlin/com/tts/kotlininvent/controller/FruitController.kt
+++ src/main/kotlin/com/tts/kotlininvent/controller/FruitController.kt
@@ -2,40 +2,38 @@ package com.tts.kotlininvent.controller
 
 import com.tts.kotlininvent.FruitService
 import com.tts.kotlininvent.model.Fruit
-import com.tts.kotlininvent.repo.FruitRepository
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
+
 @RestController
-@CrossOrigin
 @RequestMapping("/fruits")
-class FruitController(val fruitService: FruitService){
+class FruitController(private val fruitService: FruitService){
+
 
     @GetMapping("/all")
-    fun findAll() {
-        fruitService.findAll()
-    }
+    fun findAll()
+            = fruitService.findAll()
+
 
     @GetMapping("/{id}")
-    fun findFruitById(@PathVariable id: Long) {
-        fruitService.findFruitById(id)
-    }
+    fun findFruitById(@PathVariable id: Long)
+            = fruitService.findFruitById(id)
 
 
     @DeleteMapping("/delete/{id}")
-    fun deleteFruitById(@PathVariable id: Long){
-        fruitService.deleteFruitById(id)
-    }
+    fun deleteFruitById(@PathVariable id: Long)
+            = fruitService.deleteFruitById(id)
+
 
     @PutMapping("/put/{id}")
-    fun updateFruit (@PathVariable id: Long, @RequestBody fruit: Fruit){
-        fruitService.updateFruit(id, fruit)
+    fun updateFruit (@PathVariable id: Long, @RequestBody fruit: Fruit):Fruit{
+        fruit.id = id
+        return fruitService.updateFruit(fruit)
     }
+
 
     @PostMapping("/post")
-    fun addFruit (@RequestBody fruit: Fruit){
-        fruitService.addFruit(fruit)
-    }
-
+    fun addFruit ( @RequestBody fruit: Fruit)
+            = fruitService.addFruit(fruit)
 
 }
